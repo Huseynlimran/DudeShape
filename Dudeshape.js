fded914ad1dcbd724a520d9c3e624afbfb7d4bf9
Imran <89724435+Huseynlimran@users.noreply.github.com>
2021-12-19 13:25:03 +0400

    Add files via upload

---
+++ Dudeshape.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal('.top',{
+    origin: 'top' , duration: 1500, distance: '60px', opacity : 0
+})
+ScrollReveal().reveal('.bottom',{
+    origin: 'bottom' , duration: 1000, distance: '60px',opacity : 0
+})
+ScrollReveal().reveal('.left',{
+    origin: 'left' , duration: 1000, distance: '60px', opacity : 0
+})
+
+ScrollReveal().reveal('.sag',{
+    origin: 'right' , duration: 1500, distance: '60px', opacity : 0
+})
+ScrollReveal().reveal('.sol',{
+    origin: 'left' , duration: 1500, distance: '60px', opacity : 0
+})
+
+ScrollReveal().reveal('.delaa',{
+    origin: 'left' , duration: 1500, distance: '60px',  interval:100, opacity : 0
+})
+
+ScrollReveal().reveal('.dela',{
+    origin: 'right' , duration: 1500, distance: '60px',  interval:50, opacity : 0
+})
+
